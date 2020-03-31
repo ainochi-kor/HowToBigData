11062ce86f17121497b2d0955c33d2f1c3e02ffe
unknown <kthrkdals@gmail.com>
2020-04-01 03:47:51 +0900

    add 3week

--- Assignment/Assignment_04_m02.py
+++ Assignment/Assignment_04_m02.py
@@ -1,1 +1,22 @@
-import 
+#!/usr/bin/env python3
+
+list1 = [1,2,3,4,5]
+list2 = ['a', 'b', 'c', 'd', {}]
+
+#리스트의 길이가 같은지
+if len(list1) == len(list2):
+    print("len(list1) = {}".format(len(list1)))
+    print("len(list2) = {}".format(len(list2)))
+    print("두 list의 길이가 같습니다.")
+else:
+    print("두 list의 길이가 다릅니다.")
+
+'''
+for문과 위치 인덱스, 그리고 고유한 문자열을 지닌 리스트의 
+각 값들이 이미 딕셔너리에 있는 키인지 확인하는 if문을 작성한다.
+
+'''
+if
+
+
+
---
+++ Assignment/Assignment_04_m03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# CSV 파일 작성하기
+import pandas as pd
+
+sum_list = [[1,2,3,4,5],['a', 'b', 'c', 'd', 'e']]
+data_frame = pd.DataFrame(sum_list)
+
+print("Output : Output appended to file")
+
+
+
+
+
